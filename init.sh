dae5564472365526408d51c09f9df763ea1af5b9
Ibson <ibsonson@gmail.com>
2016-11-22 19:06:46 +0300

    add all files

--- init.sh
+++ init.sh
@@ -15,4 +15,4 @@ ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 ln -s /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 service nginx restart
 /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello:app &
+#gunicorn -b 0.0.0.0:8080 hello:app &
